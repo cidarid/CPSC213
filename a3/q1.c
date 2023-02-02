9bd8a98d9edb2fc9f894662c500a82653fdd9f7a
locelus <spfletcher2003@gmail.com>
2023-02-01 22:02:58 -0800

    Should be totally working now, equal with C results

--- a3/q1.c
+++ a3/q1.c
@@ -1,30 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // YOU: Allocate these global variables, using these names
-int  s, b;
+int s, b;
 int* y;
-int  q[10];
+int q[10] = {16, 32, 5, 4, 21, 1, 0, 0, 0, 0};
 
-int main (int argv, char** argc) {
-  // Ignore this block of code
-  if (argv != 11) {
-    fprintf (stderr, "usage: q[0] ... q[9]\n");
-    exit (EXIT_FAILURE);
-  }
-  for (int j=0; j<10; j++)
-    q[j] = atol (argc[1 + j]);
+int main(int argv, char** argc) {
+    // Ignore this block of code
+    /*if (argv != 11) {
+        fprintf(stderr, "usage: q[0] ... q[9]\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int j = 0; j < 10; j++) q[j] = atol(argc[1 + j]);*/
 
-  // YOU: Implement this code
-  s  = q[5];
-  s  = q[s];
-  y  = &b;
-  *y = 6;
-  y  = &q[q[2]];
-  *y = *y + q[3];
+    // YOU: Implement this code
+    s = q[5];
+    s = q[s];
+    y = &b;
+    *y = 6;
+    y = &q[q[2]];
+    *y = *y + q[3];
 
-  // Ignore this block of code
-  printf ("s=%d b=%d y=&q[%d] q={", s, b, y - q);
-  for (int j=0; j<10; j++)
-    printf("%d%s", q[j], j<9? ", ": "}\n");
-  }
+    // Ignore this block of code
+    printf("s=%d b=%d y=&q[%d] q={", s, b, y - q);
+    for (int j = 0; j < 10; j++) printf("%d%s", q[j], j < 9 ? ", " : "}\n");
+}
