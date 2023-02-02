b1463db47e1985bdec3aa25d9a9bced561e9b457
locelus <spfletcher2003@gmail.com>
2023-02-01 19:03:50 -0800

    Allocating variables

--- a3/q1.s
+++ a3/q1.s
@@ -1,1 +1,20 @@
+.pos 0x100
+                     
 
+
+.pos 0x1000
+s:                   .long 0x0
+b:                   .long 0x0
+y:                   .long 0x0
+
+.pos 0x2000
+y:                   .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
+                     .long 0x0
