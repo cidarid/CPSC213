cb85cd8e751d63483e7c27f1fa33af22fc69870d
locelus <spfletcher2003@gmail.com>
2023-02-04 15:08:40 -0800

    Added comments to bubble_sort_static

--- a3/bubble_sort_static.c
+++ a3/bubble_sort_static.c
@@ -1,12 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+// Array of values to sort
 int val[4];
 
-void sort (int n) {
+void sort(int n) {
+  // Temp val,
   int t;
-  for (int i=n-1; i>0; i--)
-    for (int j=i-1; j>=0; j--)
+  // Loop from back to front of array using i as position
+  for (int i = n - 1; i > 0; i--)
+    // Loop through all numbers before val[i], if any numbers encountered are
+    // greater than val[i], swap val[i] and val[j]
+    for (int j = i - 1; j >= 0; j--)
       if (val[i] < val[j]) {
         t = val[i];
         val[i] = val[j];
@@ -14,22 +19,24 @@ void sort (int n) {
       }
 }
 
-int main (int argc, char** argv) {
+int main(int argc, char** argv) {
   char* ep;
-  int   n;
+  int n;
   n = argc - 1;
   if (n > 4) {
-    fprintf (stderr, "Static limit of 4 numbers\n");
+    fprintf(stderr, "Static limit of 4 numbers\n");
     return -1;
   }
-  for (int i=0; i<n; i++) {
-    val[i] = strtol (argv[i+1], &ep, 10);
+  // Checks to make sure all inputs are numbers I'm pretty sure, if yes ends
+  // program
+  for (int i = 0; i < n; i++) {
+    val[i] = strtol(argv[i + 1], &ep, 10);
     if (*ep) {
-      fprintf (stderr, "Argument %d is not a number\n", i);
+      fprintf(stderr, "Argument %d is not a number\n", i);
       return -1;
     }
   }
-  sort (n);
-  for (int i=0; i<n; i++)
-    printf ("%d\n", val[i]);
+  sort(n);
+  // Prints sorted array
+  for (int i = 0; i < n; i++) printf("%d\n", val[i]);
 }
