f8e49778f6b4e66d6eebdcc4ac629d3d6ea3f7a9
locelus <spfletcher2003@gmail.com>
2023-02-21 18:09:55 -0800

    Updating gitignore, cleaning up a2

--- a2/S2-global-dyn-array.java
+++
@@ -1,9 +0,0 @@
-public class Foo {
-  static int a;
-  static int b[] = new int[10];
-  
-  void foo () {
-    a=0;
-    b[a]=a;
-  }
-}
--- a2/scratch.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-  int* p;
-  int a[10];
-  p = a + 5;
-  *p = 30;
-}
