45e36bb461f3b7e210b5e28454bf1e3bac8a928b
locelus <spfletcher2003@gmail.com>
2023-03-12 17:19:01 -0700

    Comment reformat

--- a7/examples/e4.c
+++ a7/examples/e4.c
@@ -5,8 +5,9 @@
 /* simulate exec using the C function system */
 /* this function is not in the assembly */
 int exec(char *buffer, int size) {
-  /* buffer might not be null-terminated, but system() wants a null-terminated string,
-     so our emulated exec adds null-termination here using a temporary variable */
+  /* buffer might not be null-terminated, but system() wants a null-terminated
+     string, so our emulated exec adds null-termination here using a temporary
+     variable */
   char *str = malloc(size + 1);
   memcpy(str, buffer, size);
   str[size] = 0;
