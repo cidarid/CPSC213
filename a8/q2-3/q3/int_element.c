b0463fe9e099cee3dde2289205fed58e2c69dba3
locelus <spfletcher2003@gmail.com>
2023-03-18 15:40:44 -0700

    no seg faults woohoo

--- a8/q2-3/q3/int_element.c
+++ a8/q2-3/q3/int_element.c
@@ -11,7 +11,7 @@ functions.
 You may add your own private functions here too. */
 
 struct int_class {
-  struct int_element (*int_element_new)(int value);
+  struct int_element *(*int_element_new)(int value);
   void (*print)(struct element *);
   int (*compare)(struct element *, struct element *);
   int (*int_element_get_value)(struct int_element *);
@@ -33,17 +33,26 @@ struct int_element *int_element_new(int value) {
 }
 
 /* Print this element (without any trailing newline) */
-void print(struct element *);
+void print(struct element *element) {
+  struct int_element *real = (struct int_element *)element;
+  printf("%d", real->value);
+}
 /* Compare two elements. int_element should always compare LESS than
  * str_element. The compare function should return: a number < 0 if the first
  * element is less than the second element, 0 if the elements compare equal, a
  * number > 0 otherwise.
  */
-int compare(struct element *, struct element *);
+int compare(struct element *a, struct element *b) {
+  return 0;
+}
 /* Static function that obtains the value held in an int_element. */
-int int_element_get_value(struct int_element *);
+int int_element_get_value(struct int_element *int_element) {
+  return 0;
+}
 /* Static function that determines whether this is an int_element. */
-int is_int_element(struct element *);
+int is_int_element(struct element *element) {
+  return 0;
+}
 
 struct int_class int_class_table = {
     int_element_new, print, compare, int_element_get_value, is_int_element,
