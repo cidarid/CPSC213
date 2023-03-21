2fd9c25d2d69b26ffb1e3c8db0378868610e777a
locelus <spfletcher2003@gmail.com>
2023-03-20 19:47:51 -0700

    Boilerplate

---
+++ a9/q3/treasureHunt.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <assert.h>
+#include "uthread.h"
+#include "queue.h"
+#include "disk.h"
+
+queue_t pending_read_queue;
+
+void interrupt_service_routine() {
+  // TODO
+}
+
+void handleOtherReads(void *resultv, void *countv) {
+  // TODO
+}
+
+void handleFirstRead(void *resultv, void *countv) {
+  // TODO
+}
+
+int main(int argc, char **argv) {
+  // Command Line Arguments
+  static char* usage = "usage: treasureHunt starting_block_number";
+  int starting_block_number;
+  char *endptr;
+  if (argc == 2)
+    starting_block_number = strtol (argv [1], &endptr, 10);
+  if (argc != 2 || *endptr != 0) {
+    printf ("argument error - %s \n", usage);
+    return EXIT_FAILURE;
+  }
+
+  // Initialize
+  uthread_init (1);
+  disk_start (interrupt_service_routine);
+  pending_read_queue = queue_create();
+
+
+  // Start the Hunt
+  // TODO
+  while (1); // infinite loop so that main doesn't return before hunt completes
+}
