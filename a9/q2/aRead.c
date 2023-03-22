ae49688e306dc16b29ec570b5bc1d22352572364
Morgan Dean <morgandeanhi@gmail.com>
2023-03-22 14:45:22 -0700

    start on aRead

--- a9/q2/aRead.c
+++ a9/q2/aRead.c
@@ -1,54 +1,58 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <sys/errno.h>
-#include <assert.h>
-#include "queue.h"
 #include "disk.h"
+#include "queue.h"
 #include "uthread.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
 
-queue_t      pending_read_queue;
+queue_t pending_read_queue;
 unsigned int sum = 0;
 volatile int pending_reads;
 
-void interrupt_service_routine () {
-  void* val;
-  void (*callback)(void*,void*);
-  queue_dequeue (pending_read_queue, &val, NULL, &callback);
-  callback (val, NULL);
+void interrupt_service_routine() {
+  void *val;
+  void (*callback)(void *, void *);
+  queue_dequeue(pending_read_queue, &val, NULL, &callback);
+  callback(val, NULL);
 }
 
-void handle_read (void* resultv, void* not_used) {
-  // TODO add result to sum
+void handle_read(void *resultv, void *not_used) {
+  sum += *(int *)resultv;
+  pending_reads--;
 }
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv) {
 
   // Command Line Arguments
-  static char* usage = "usage: aRead num_blocks";
+  static char *usage = "usage: aRead num_blocks";
   int num_blocks;
   char *endptr;
   if (argc == 2)
-    num_blocks = strtol (argv [1], &endptr, 10);
+    num_blocks = strtol(argv[1], &endptr, 10);
   if (argc != 2 || *endptr != 0) {
-    printf ("argument error - %s \n", usage);
+    printf("argument error - %s \n", usage);
     return EXIT_FAILURE;
   }
 
   // Initialize
-  uthread_init (1);
-  disk_start (interrupt_service_routine);
+  uthread_init(1);
+  disk_start(interrupt_service_routine);
   pending_read_queue = queue_create();
   pending_reads = num_blocks;
 
   // Trigger reads and sum blocks
-  // TODO
+  for (int i = 0; i < num_blocks; i++) {
+    int *res = 0;
+    queue_enqueue(pending_read_queue, res, NULL, handle_read);
+    disk_schedule_read(res, i);
+  }
 
   // polling loop so that main doesn't return before all of the reads complete
-  while (pending_reads > 0);
+  while (pending_reads > 0)
+    ;
 
-  printf ("%d\n", sum);
+  printf("%d\n", sum);
 }
-
-
