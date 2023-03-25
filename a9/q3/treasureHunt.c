6e1b7e54863c0c5e883b87b7d61881fddce8c172
locelus <spfletcher2003@gmail.com>
2023-03-25 14:23:20 -0700

    VS reformatting

--- a9/q3/treasureHunt.c
+++ a9/q3/treasureHunt.c
@@ -1,8 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/errno.h>
-#include <assert.h>
+
+#include "disk.h"
+#include "queue.h"
 #include "uthread.h"
 #include "queue.h"
 #include "disk.h"
@@ -23,19 +27,18 @@ void handleFirstRead(void *resultv, void *countv) {
 
 int main(int argc, char **argv) {
   // Command Line Arguments
-  static char* usage = "usage: treasureHunt starting_block_number";
+  static char *usage = "usage: treasureHunt starting_block_number";
   int starting_block_number;
   char *endptr;
-  if (argc == 2)
-    starting_block_number = strtol (argv [1], &endptr, 10);
+  if (argc == 2) starting_block_number = strtol(argv[1], &endptr, 10);
   if (argc != 2 || *endptr != 0) {
-    printf ("argument error - %s \n", usage);
+    printf("argument error - %s \n", usage);
     return EXIT_FAILURE;
   }
 
   // Initialize
-  uthread_init (1);
-  disk_start (interrupt_service_routine);
+  uthread_init(1);
+  disk_start(interrupt_service_routine);
   pending_read_queue = queue_create();
 
 
