a9cb48e84d0b8073ecaf4bd32ed1079f98ab7023
locelus <spfletcher2003@gmail.com>
2023-04-02 22:11:20 -0700

    Works!

--- a9/q9.2/tRead.c
+++ a9/q9.2/tRead.c
@@ -1,45 +1,83 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <sys/errno.h>
 #include <assert.h>
-#include "queue.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+
 #include "disk.h"
+#include "queue.h"
 #include "uthread.h"
 
-queue_t      pending_read_queue;
+queue_t pending_read_queue;
+volatile int result;
 unsigned int sum = 0;
+int* vals;
+uthread_t* threads;
+uthread_t active;
 
-void interrupt_service_routine () {
-  // TODO
+void interrupt_service_routine() {
+  void* thread;
+  void (*callback)(void*, void*);
+  queue_dequeue(pending_read_queue, &thread, NULL, &callback);
+  callback(thread, NULL);
+  /*void* val;
+  void (*callback)(void*, void*);
+  uthread_unblock() queue_dequeue(pending_read_queue, &val, NULL, &callback);
+  callback(val, NULL);*/
 }
 
-void* read_block (void* blocknov) {
-  // TODO schedule read and the update (correctly)
-
+void* read_block(void* blocknov) {
+  uthread_t waiting_thread = uthread_self();
+  int blockno = *(int*)blocknov;
+  disk_schedule_read(&vals[blockno], blockno);
+  uthread_block();
+  sum += vals[blockno];
   return NULL;
 }
 
-int main (int argc, char** argv) {
+void unblock(void* _thread, void* none) {
+  uthread_t* thread = _thread;
+  uthread_unblock(*thread);
+}
 
+int main(int argc, char** argv) {
   // Command Line Arguments
   static char* usage = "usage: tRead num_blocks";
   int num_blocks;
-  char *endptr;
-  if (argc == 2)
-    num_blocks = strtol (argv [1], &endptr, 10);
+  char* endptr;
+  if (argc == 2) num_blocks = strtol(argv[1], &endptr, 10);
   if (argc != 2 || *endptr != 0) {
-    printf ("argument error - %s \n", usage);
+    printf("argument error - %s \n", usage);
     return EXIT_FAILURE;
   }
 
   // Initialize
-  uthread_init (1);
-  disk_start (interrupt_service_routine);
+  uthread_init(1);
+  disk_start(interrupt_service_routine);
   pending_read_queue = queue_create();
+  threads = malloc(sizeof(uthread_t) * num_blocks);
+  vals = malloc(sizeof(int) * num_blocks);
 
-  // Sum Blocks
+  int val = 0;
+  int val_2 = val + 1;
+  // active = uthread_create(read_block, &val);
+  for (int i = 0; i < num_blocks; i++) {
+    vals[i] = i;
+    threads[i] = uthread_create(read_block, &vals[i]);
+    queue_enqueue(pending_read_queue, &threads[i], NULL, unblock);
+  }
+  for (int i = 0; i < num_blocks; i++) {
+    uthread_join(threads[i], NULL);
+  }
+  printf("%d\n", sum);
+  free(threads);
+  free(vals);
+  /*uthread_t local = uthread_create(read_block, &val);
+  queue_enqueue(pending_read_queue, &local, NULL, unblock);
+  uthread_t local_2 = uthread_create(read_block, &val_2);
+  queue_enqueue(pending_read_queue, &local_2, NULL, unblock);
+  uthread_join(local, NULL);
+  uthread_join(local_2, NULL);*/
   // TODO
 }
-
